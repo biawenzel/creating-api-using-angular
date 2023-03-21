ca40bbd5ba949faa56deddab184b5a5d89ca64fd
biawenzel <biahwenzel@gmail.com>
2023-03-21 11:22:49 -0300

    creating curso component

---
+++ src/app/curso/curso.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curso',
+  templateUrl: './curso.component.html',
+  styleUrls: ['./curso.component.css']
+})
+export class CursoComponent implements OnInit {
+
+  nome:string = "Bia";
+
+  //construtor
+  constructor() {}
+
+  //inicializador
+  ngOnInit(): void {
+
+  }
+}
